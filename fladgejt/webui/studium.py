17496c06bee26a94fdfcdd88bb7c750e13779e37
Kristian Valentin <valentin.kristian@gmail.com>
2014-08-27 20:54:14 +0200

    Add support for adding and deleting enrollment list.

--- fladgejt/webui/studium.py
+++ fladgejt/webui/studium.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 
-from aisikl.app import Application
-from fladgejt.helpers import memoized, find_row, with_key_args
+from aisikl.app import Application, assert_ops
+from aisikl.exceptions import AISBehaviorError
+from fladgejt.helpers import memoized, find_row, find_option, with_key_args
 from fladgejt.structures import Studium, ZapisnyList, Hodnotenie
 
 
@@ -30,7 +32,7 @@ class WebuiStudiumMixin:
     def get_zapisne_listy(self, studium_key):
         app = self._open_administracia_studia()
 
-        self.__vyber_zapisny_list(app, studium_key, None)
+        self.__vyber_studium(app, studium_key)
         result = [ZapisnyList(akademicky_rok=row['popisAkadRok'],
                               rocnik=row['rokRocnik'],
                               sp_skratka=row['studProgramSkratka'],
@@ -39,24 +41,29 @@ class WebuiStudiumMixin:
                   for row in app.d.zapisneListyTable.all_rows()]
         return result
 
-    def __vyber_zapisny_list(self, app, studium_key, zapisny_list_key):
-        # Ak este nie je vybrate spravne studium, vyberieme ho a stlacime
-        # nacitavaci button (sipku dole).
+    def __vyber_studium(self, app, studium_key):
         sp_skratka, zaciatok = studium_key
+
+        # Najdeme studium.
         studium_index = find_row(
             app.d.studiaTable.all_rows(),
             studijnyProgramSkratka=sp_skratka,
             zaciatokStudia=zaciatok)
+
+        # Vyberieme studium a stlacime nacitavaci button (sipku dole).
         if app.d.studiaTable.selected_row_indexes != [studium_index] or len(app.d.zapisneListyTable.all_rows()) == 0:
             app.d.studiaTable.select(studium_index)
             app.d.nacitatButton.click()
 
+    def __vyber_zapisny_list(self, app, studium_key, zapisny_list_key):
+        # Ak este nie je vybrate spravne studium, vyberieme ho.
+        self.__vyber_studium(app, studium_key)
+
         # Vyberieme zapisny list.
-        if zapisny_list_key is not None:
-            (akademicky_rok,) = zapisny_list_key
-            zapisny_list_index = find_row(
-                app.d.zapisneListyTable.all_rows(), popisAkadRok=akademicky_rok)
-            app.d.zapisneListyTable.select(zapisny_list_index)
+        (akademicky_rok,) = zapisny_list_key
+        zapisny_list_index = find_row(
+            app.d.zapisneListyTable.all_rows(), popisAkadRok=akademicky_rok)
+        app.d.zapisneListyTable.select(zapisny_list_index)
 
     @memoized
     def _open_terminy_hodnotenia_app(self, studium_key, zapisny_list_key):
@@ -95,7 +102,7 @@ class WebuiStudiumMixin:
         app = self._open_administracia_studia()
 
         # Vyberieme spravne studium.
-        self.__vyber_zapisny_list(app, studium_key, None)
+        self.__vyber_studium(app, studium_key)
 
         # Stlacime v menu "Kontrola" -> "Ziskanych kreditov, prehlad hodnotenia".
         with app.collect_operations() as ops:
@@ -125,3 +132,105 @@ class WebuiStudiumMixin:
         app.awaited_close_dialog(ops)
 
         return result
+
+    def __open_novy_zapisny_list_dialog(self, app, studium_key):
+        # Vyberieme studium.
+        self.__vyber_studium(app, studium_key)
+
+        # Klikneme na tlacidlo pridania noveho zapisneho listu.
+        with app.collect_operations() as ops:
+            app.d.pridatZapisnyListButton.click()
+        app.awaited_open_dialog(ops)
+
+    def get_akademicke_roky_noveho_zapisneho_listu(self, studium_key):
+        '''Gets available values for the academic year option when creating a
+        new enrollment list.
+
+        Args:
+            studium_key: studium identifier
+
+        Returns:
+            A list of :class: `~Option` objects.
+        '''
+        app = self._open_administracia_studia()
+
+        self.__open_novy_zapisny_list_dialog(app, studium_key)
+        options = app.d.rokComboBox.options
+
+        with app.collect_operations() as ops:
+            app.d.closeButton.click()
+        app.awaited_close_dialog(ops)
+
+        return options
+
+    def get_roky_studia_noveho_zapisneho_listu(self, studium_key):
+        '''Gets available values for the study year option when creating a
+        new enrollment list.
+
+        Args:
+            studium_key: studium identifier
+
+        Returns:
+            A list of :class: `~Option` objects.
+        '''
+        app = self._open_administracia_studia()
+
+        self.__open_novy_zapisny_list_dialog(app, studium_key)
+        options = app.d.rocnikComboBox.options
+
+        with app.collect_operations() as ops:
+            app.d.closeButton.click()
+        app.awaited_close_dialog(ops)
+
+        return options
+
+    def create_zapisny_list(self, studium_key, akademicky_rok, rok_studia):
+        '''Creates enrollment list.
+
+        Args:
+            studium_key: studium identifier
+            akademicky_rok: Academic year of the time range in the form of
+                'start/end'. E.g. '2013/2014'. (optional)
+            rok_studia: year of the study
+        '''
+        app = self._open_administracia_studia()
+
+        self.__open_novy_zapisny_list_dialog(app, studium_key)
+
+        # V combo boxe vyberieme rok studia.
+        app.d.rocnikComboBox.select(find_option(app.d.rocnikComboBox.options, id=str(rok_studia)))
+
+        # Ak je nastaveny akademicky rok, vyberieme ho v combo boxe.
+        if akademicky_rok is not None:
+            app.d.rokComboBox.select(find_option(app.d.rokComboBox.options, id=akademicky_rok))
+
+        # Stlacime tlacidlo "OK".
+        with app.collect_operations() as ops:
+            app.d.enterButton.click()
+
+        # Dialog sa zavrie a vyskoci message box, ze cinnost bola uspesne dokoncena.
+        assert_ops(ops, 'closeDialog', 'messageBox')
+        app.close_dialog(app.d.name)
+        if ops[1].args[0] != 'Činnosť úspešne dokončená.':
+            raise AISBehaviorError("AIS displayed an error: {}".format(ops))
+
+    def delete_zapisny_list(self, studium_key, zapisny_list_key):
+        '''Deletes enrollment list.
+
+        Args:
+            studium_key: studium identifier
+            zapisny_list_key: enrollment list identifier
+        '''
+        app = self._open_administracia_studia()
+
+        # Vyberieme zapisny list.
+        self.__vyber_zapisny_list(app, studium_key, zapisny_list_key)
+
+        # Klikneme na tlacidlo zrusenia zapisneho listu.
+        with app.collect_operations() as ops:
+            app.d.odobratZapisnyListButton.click()
+
+        # Vyskoci confirm box, ci naozaj chceme odstranit zapisny list. Stlacime "Ano".
+        assert_ops(ops, 'confirmBox')
+        with app.collect_operations() as ops:
+            app.confirm_box(2)
