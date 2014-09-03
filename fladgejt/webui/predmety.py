3217d8be3581982ddc288b1c4857e623418f83b7
ddeath <dusan.plavak@gmail.com>
2014-09-03 05:16:02 +0200

    Add function for searching subjects.

--- fladgejt/webui/predmety.py
+++ fladgejt/webui/predmety.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 
 from aisikl.app import Application, assert_ops
-from fladgejt.helpers import memoized, find_option
+from fladgejt.helpers import memoized, find_option, find_row
+from fladgejt.structures import Predmet
 
 class WebuiPredmetyMixin:
     @memoized
@@ -9,6 +11,46 @@ class WebuiPredmetyMixin:
         app, ops = Application.open(self.context, url)
         app.awaited_open_main_dialog(ops)
         return app
+
+    def __prepare_dialog(self):
+        app = self._open_register_predmetov()
+
+        if app.d.fakultaUniverzitaComboBox.selected_index != 0:
+            app.d.fakultaUniverzitaComboBox.select(0)
+
+        if app.d.semesterComboBox.selected_index != 0:
+            app.d.semesterComboBox.select(0)
+
+        if app.d.stupenPredmetuComboBox.selected_index != 0:
+            app.d.stupenPredmetuComboBox.select(0)
+
+        if app.d.typPredmetuComboBox.selected_index != 0:
+            app.d.typPredmetuComboBox.select(0)
+
+        if app.d.zobrazitLenComboBox.selected_index != 0:
+            app.d.zobrazitLenComboBox.select(0)
+
+        if app.d.skratkaPredmetuTextField.value != '':
+            app.d.skratkaPredmetuTextField.write('')
+
+        if app.d.nazovPredmetuTextField.value != '':
+            app.d.nazovPredmetuTextField.write('')
+
+        if app.d.cisloPredmetuNumberControl.bdvalue != '':
+            app.d.cisloPredmetuNumberControl.write('')
+
+        if app.d.strediskoTextField.is_editable():
+            app.d.strediskoTextField.write('')
+        else:
+            app.d.zmazatStrediskoButton.click()
+
+        if app.d.skratkaStudProgramuTextField.is_editable():
+            app.d.skratkaStudProgramuTextField.write('')
+        else:
+            app.d.zmazatStudProgramButton.click()
+
+        if app.d.odobratVyucujucehoButton.is_really_enabled():
+            app.d.odobratVyucujucehoButton.click()
 
     def get_informacny_list(self, kod_predmetu, akademicky_rok=None):
         app = self._open_register_predmetov()
@@ -56,3 +98,207 @@ class WebuiPredmetyMixin:
         # Otvori sa vysledne PDF.
         url = app.awaited_shell_exec(ops)
         return app.context.request_text(url)
+
+    def vyhladaj_predmety(self, fakulta=None, stredisko=None, skratka_sp=None, skratka_predmetu=None, nazov_predmetu=None, akademicky_rok=None, semester=None, stupen=None):
+        app = self._open_register_predmetov()
+        self.__prepare_dialog()
+
+        if fakulta is not None:
+            index = find_option(app.d.fakultaUniverzitaComboBox.options, id=fakulta)
+            app.d.fakultaUniverzitaComboBox.select(index)
+            
+        if semester is not None:
+            index = find_option(app.d.semesterComboBox.options, title=semester)
+            app.d.semesterComboBox.select(index)
+            
+
+        if akademicky_rok is None:
+            index = 0
+        else:
+            index = find_option(app.d.akRokComboBox.options, title=akademicky_rok)
+        app.d.akRokComboBox.select(index)
+
+        if stredisko is not None:
+            # Napiseme skratku strediska do textfieldu, ak je jednoznacna
+            # AIS ju automaticky vyberie.
+            app.d.strediskoTextField.write(stredisko)
+
+            # Na zaklade napisanej skratky skusime vybrat stredisko.
+            # Ak je viacero skratiek s rovnakym prefixom, alebo taka
+            # skratka neexistuje otvori sa dialog so strediskami.
+            with app.collect_operations() as ops:
+                app.d.vybratStrediskoAction.execute()
+
+            # Ak bola skratka nejednoznacna
+            if len(ops) == 1:
+                assert_ops(ops, 'openDialog')
+                
+                if ops[0].args[1] != 'Zabezpečujúce strediská':
+                    raise AISBehaviorError("AIS opened an unexpected dialog: {}".format(ops))
+
+                app.awaited_open_dialog(ops)
+
+                # Skusime najst skratku medzi strediskami v tabulke
+                index = find_row(app.d.table.all_rows(), skratka=stredisko)
+
+                app.d.table.select(index)
+
+                # Stlacime zatvaraci button.
+                with app.collect_operations() as ops:
+                    app.d.enterButton.click()
+
+                # Dialog sa zavrie.
+                app.awaited_close_dialog(ops)
+
+        if skratka_sp is not None:
+            # Napiseme skratku studijneho programu do textfieldu, 
+            # ak je jednoznacna AIS ju automaticky vyberie.
+            app.d.skratkaStudProgramuTextField.write(skratka_sp)
+
+            # Na zaklade napisanej skratky skusime vybrat studijny program.
+            # Ak je viacero skratiek s rovnakym prefixom, alebo taka
+            # skratka neexistuje otvori sa dialog so studijnymi programami.
+            with app.collect_operations() as ops:
+                app.d.vybratStudProgramAction.execute()
+
+            if len(ops) == 1:
+                assert_ops(ops, 'openDialog')
+                
+                if ops[0].args[1] != 'Študijné programy':
+                    raise AISBehaviorError("AIS opened an unexpected dialog: {}".format(ops))
+
+                app.awaited_open_dialog(ops)
+                # Skusime najst skratku medzi studijnymi programami v tabulke
+                index = find_row(app.d.table.all_rows(), skratka=skratka_sp)
+
+                app.d.table.select(index)
+
+                # Stlacime zatvaraci button.
+                with app.collect_operations() as ops:
+                    app.d.enterButton.click()
+
+                # Dialog sa zavrie.
+                app.awaited_close_dialog(ops)
+
+
+        if skratka_predmetu is not None:
+            app.d.skratkaPredmetuTextField.write(skratka_predmetu)
+
+        if nazov_predmetu is not None:
+            app.d.nazovPredmetuTextField.write(nazov_predmetu)
+
+        if stupen is not None:
+            index = find_option(app.d.stupenPredmetuComboBox.options, title=stupen)
+            app.d.stupenPredmetuComboBox.select(index)
+
+        # Uz mame nastavene vsetky parametre vyhladavania, stlacime nacitavaci button.
+        with app.collect_operations() as ops:
+            app.d.zobrazitPredmetyButton.click()
+
+        message = None
+        if len(ops) == 1:
+            assert_ops(ops, 'messageBox')
+            message = ops[0].args[0]            
+
+        while not app.d.zoznamPredmetovTable.is_end_of_data:
+            app.d.zoznamPredmetovTable.scroll_down(10)
+        if app.d.zoznamPredmetovTable.truncated:
+            message = "Neboli načítané všetky dáta. Upresnite prosím kritéria vyhľadávania."
+
+        if app.d.zoznamPredmetovTable.loaded_rows:
+            predmety = [Predmet(skratka=row['skratka'],
+                              nazov=row['nazov'],
+                              typ_vyucby=row['kodTypPredmetu'],
+                              semester=row['kodSemester'],
+                              kredit=row['kredit'])
+                      for row in app.d.zoznamPredmetovTable.loaded_rows]
+        else:
+            predmety = None
+
+        return [predmety, message]
+
+    def get_fakulta_options(self):
+        app = self._open_register_predmetov()
+        self.__prepare_dialog()
+
+        return app.d.fakultaUniverzitaComboBox.options
+
+    def get_akademicky_rok_options(self):
+        app = self._open_register_predmetov()
+        self.__prepare_dialog()
+
+        return app.d.akRokComboBox.options
+
+    def get_stredisko_options(self, fakulta=None):
+        app = self._open_register_predmetov()
+        self.__prepare_dialog()
+
+        if fakulta is None:
+            index = 0
+        else:
+            index = find_option(app.d.fakultaUniverzitaComboBox.options, id=fakulta)
+        app.d.fakultaUniverzitaComboBox.select(index)
+
+        # Otvorime dialog so strediskami
+        with app.collect_operations() as ops:
+            app.d.vybratStrediskoAction.execute()
+
+        assert_ops(ops, 'openDialog')
+        
+        if ops[0].args[1] != 'Zabezpečujúce strediská':
+            raise AISBehaviorError("AIS opened an unexpected dialog: {}".format(ops))
+
+        app.awaited_open_dialog(ops)
+
+        message = None
+        while not app.d.table.is_end_of_data:
+            app.d.table.scroll_down(10)
+        if app.d.table.truncated:
+            message = "Neboli načítané všetky dáta. Upresnite prosím kritéria vyhľadávania."
+        result = [app.d.table.loaded_rows, message]
+
+        # Stlacime zatvaraci button pre strediskovy dialog.
+        with app.collect_operations() as ops:
+            app.d.enterButton.click()
+
+        # Dialog sa zavrie.
+        app.awaited_close_dialog(ops)
+
+        return result
+
+    def get_studijny_program_options(self, fakulta=None):
+        app = self._open_register_predmetov()
+        self.__prepare_dialog()
+
+        if fakulta is None:
+            index = 0
+        else:
+            index = find_option(app.d.fakultaUniverzitaComboBox.options, id=fakulta)
+        app.d.fakultaUniverzitaComboBox.select(index)
+
+        # Otvorime dialog so studijnymi programami
+        with app.collect_operations() as ops:
+            app.d.vybratStudProgramAction.execute()
+
+        assert_ops(ops, 'openDialog')
+        
+        if ops[0].args[1] != 'Študijné programy':
+            raise AISBehaviorError("AIS opened an unexpected dialog: {}".format(ops))
+
+        app.awaited_open_dialog(ops)
+
+        message = None
+        while not app.d.table.is_end_of_data:
+            app.d.table.scroll_down(10)
+        if app.d.table.truncated:
+            message = "Neboli načítané všetky dáta. Upresnite prosím kritéria vyhľadávania."
+        result = [app.d.table.loaded_rows, message]
+
+        # Stlacime zatvaraci button pre strediskovy dialog.
+        with app.collect_operations() as ops:
+            app.d.enterButton.click()
+
+        # Dialog sa zavrie.
+        app.awaited_close_dialog(ops)
+
+        return result
