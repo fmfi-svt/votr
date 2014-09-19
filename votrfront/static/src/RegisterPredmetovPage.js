4019b0d7817e3e144b9d118aad9ed6d88dfdec2d
ddeath <dusan.plavak@gmail.com>
2014-09-19 23:04:29 +0200

    Add keys and beautify code.

--- votrfront/static/src/RegisterPredmetovPage.js
+++ votrfront/static/src/RegisterPredmetovPage.js
@@ -4,83 +4,107 @@
 
 
 Votr.RegisterPredmetovForm = React.createClass({
+  getInitialState: function () {
+    return {
+      stredisko: '',
+      studijnyProgramSkratka: '',
+      skratkaPredmetu: '',
+      nazovPredmetu: ''
+    };
+  },
+
   render: function () {
     var cache = new Votr.CacheRequester();
-    var fakulty = cache.get('get_fakulta_options');
-    var rocniky = cache.get('get_akademicky_rok_options');
-    var stupne = cache.get('get_stupen_options');
-    var semestre = cache.get('get_semester_options');
-
-    if(!fakulty || !rocniky || !stupne || !semestre)
-      return <Votr.Loading requests={cache.missing} />
+    var fakulty = cache.get('get_register_predmetov_fakulta_options');
+    var rocniky = cache.get('get_register_predmetov_akademicky_rok_options');
+    var stupne = cache.get('get_register_predmetov_stupen_options');
+    var semestre = cache.get('get_register_predmetov_semester_options');
 
     return <form onSubmit={this.handleSubmit}>
-        <div>
-          <label htmlFor="fakultaSelect">Fakulta: </label>
-          {fakulty ?
-            <select id="fakultaSelect" onChange={this.handleFieldChange}>
-              {fakulty.map((fakulta, index) =>
-              <option value={fakulta.id}>{fakulta.title}</option> )}
+      <div>
+        <label>
+          {"Fakulta: "}
+          {fakulty ? 
+            <select name="fakulta" value={this.state.fakulta} onChange={this.handleFieldChange}>
+              {fakulty.map((fakulta) =>
+                <option key={fakulta.id} value={fakulta.id}>{fakulta.title}</option>
+              )}
             </select> : <Votr.Loading requests={cache.missing} />}
-        </div>
-        <div>
-          <label htmlFor="strediskoInput" onChange={this.handleFieldChange}>Stredisko: </label>
-          <input id="strediskoInput" type="text" />
-        </div>
-        <div>
-          <label htmlFor="studijnyProgramInput" onChange={this.handleFieldChange}>Štúdijný program: </label>
-          <input id="studijnyProgramInput" type="text" />
-        </div>
-        <div>
-          <label htmlFor="skratkaPredmetuInput" onChange={this.handleFieldChange}>Skratka predmetu: </label>
-          <input id="skratkaPredmetuInput" type="text" />
-        </div>
-        <div>
-          <label htmlFor="nazovPredmetuInput" onChange={this.handleFieldChange}>Názov predmetu: </label>
-          <input id="nazovPredmetuInput" type="text" />
-        </div>
-        <div>
-          <label htmlFor="semesterSelect">Semester: </label>
+        </label>
+      </div>
+      <div>
+        <label>
+          {"Stredisko: "}
+          <input name="stredisko" value={this.state.stredisko} type="text" onChange={this.handleFieldChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          {"Štúdijný program: "}
+          <input name="studijnyProgramSkratka" value={this.state.studijnyProgram} type="text" onChange={this.handleFieldChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          {"Skratka predmetu: "}
+          <input name="skratkaPredmetu" value={this.state.skratkaPredmetu} type="text" onChange={this.handleFieldChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          {"Názov predmetu: "}
+          <input name="nazovPredmetu" value={this.state.nazovPredmetu} type="text" onChange={this.handleFieldChange} />
+        </label>
+      </div>
+      <div>
+        <label>
+          {"Semester: "}
           {semestre ?
-            <select id="semesterSelect" onChange={this.handleFieldChange}>
-              {semestre.map((semester, index) =>
-              <option value={semester.id}>{semester.title}</option> )})
+            <select name="semester" value={this.state.semester} onChange={this.handleFieldChange}>
+              {semestre.map((semester) =>
+                <option key={semester.id} value={semester.id}>{semester.title}</option>
+              )}
             </select> : <Votr.Loading requests={cache.missing} />}
-        </div>
-        <div>
-          <label htmlFor="akademickyRokSelect">Akademický rok: </label>
+        </label>
+      </div>
+      <div>
+        <label>
+          {"Akademický rok: "}
           {rocniky ?
-            <select id="akademickyRokSelect" onChange={this.handleFieldChange}>
-              {rocniky.map((rocnik, index) =>
-              <option value={rocnik.id}>{rocnik.title}</option> )})
+            <select name="akademickyRok" value={this.state.akademickyRok} onChange={this.handleFieldChange}>
+              {rocniky.map((rocnik) =>
+                <option key={rocnik.id} value={rocnik.id}>{rocnik.title}</option>
+              )}
             </select> : <Votr.Loading requests={cache.missing} />}
-        </div>
-        <div>
-          <label htmlFor="stupenSelect">Stupeň: </label>
-          {stupne ?
-            <select id="stupenSelect" onChange={this.handleFieldChange}>
-              {stupne.map((stupen, index) => 
-              <option value={stupen.id}>{stupen.title}</option> )})
+        </label>
+      </div>
+      <div>
+        <label>
+          {"Stupeň: "}
+          {stupne ? 
+            <select name="stupen" value={this.state.stupen} onChange={this.handleFieldChange}>
+              {stupne.map((stupen) =>
+                <option key={stupen.id} value={stupen.id}>{stupen.title}</option>
+              )}
             </select> : <Votr.Loading requests={cache.missing} />}
-        </div>
-        <div>
-          <button type="submit">Vyhľadaj</button>
-        </div>
+        </label>
+      </div>
+      <div>
+        <button type="submit">Vyhľadaj</button>
+      </div>
     </form>;
   },
+
   handleFieldChange: function (event) {
     var update = {};
-    update[event.target.id] = event.target.value;
+    update[event.target.name] = event.target.value;
     this.setState(update);
   },
+
   handleSubmit: function(event) {
     var state = this.state;
     event.preventDefault();
-    Votr.navigate({ action: 'registerPredmetov', fakulta: state.fakultaSelect,
-                    stredisko: state.strediskoInput, studijnyProgramSkratka: state.studijnyProgramInput,
-                    skratkaPredmetu: state.skratkaPredmetuInput, nazovPredmetu: state.nazovPredmetuInput,
-                    semester: state.semesterSelect, akademickyRok: state.akademickyRokSelect,
-                    stupen: state.stupenSelect });
+    Votr.navigate(_.assign({ action: 'registerPredmetov' }, this.state));
   }
 });
 
@@ -96,7 +120,7 @@ Votr.RegisterPredmetovResultTable = React.createClass({
        query.akademickyRok == undefined &&
        query.semester == undefined &&
        query.stupen == undefined )
-      return <div></div>;
+      return null;
 
     var response = cache.get('vyhladaj_predmety',
           query.fakulta || null,
@@ -111,8 +135,10 @@ Votr.RegisterPredmetovResultTable = React.createClass({
     if(!response)
       return <Votr.Loading requests={cache.missing} />
     
+    var [rows, message] = response;
+
     return <div>
-        {response[1] != null ? <div>{ response[1] }</div> : ''}
+        <div>{message}</div>
         <table>
           <tr>
             <th>Názov predmetu</th>
@@ -121,8 +147,8 @@ Votr.RegisterPredmetovResultTable = React.createClass({
             <th>Semester</th>
             <th>Počet kreditov</th>
           </tr>
-          { response[0].map((predmet, index) =>
-          <tr>
+          { rows.map((predmet) =>
+          <tr key={predmet.skratka}>
             <td>{predmet.nazov}</td>
             <td>{predmet.skratka}</td>
             <td>{predmet.typ_vyucby}</td>
@@ -138,8 +164,8 @@ Votr.RegisterPredmetovResultTable = React.createClass({
 Votr.RegisterPredmetovPage = React.createClass({
   render: function () {
     return <Votr.PageLayout query={this.props.query}>
-        <Votr.RegisterPredmetovForm query={this.props.query} component={Votr.RegisterPredmetovForm} />
-        <Votr.RegisterPredmetovResultTable query={this.props.query} component={Votr.RegisterPredmetovResultTable} />
+        <Votr.RegisterPredmetovForm query={this.props.query} />
+        <Votr.RegisterPredmetovResultTable query={this.props.query} />
     </Votr.PageLayout>;
   }
 });
