1134d628ac8867b843c3405090290d3fe1ebc15e
Tomi Belan <tomi.belan@gmail.com>
2014-10-09 23:16:57 +0200

    Don't stop the werkzeug reloader on syntax errors.

--- console.py
+++ console.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
 try:
-    import votrfront.local_settings as settings
-except ImportError as e:
-    if 'local_settings' not in str(e): raise
-    import votrfront.default_settings as settings
+    try:
+        import votrfront.local_settings as settings
+    except ImportError as e:
+        if 'local_settings' not in str(e): raise
+        import votrfront.default_settings as settings
 
-from votrfront.app import VotrApp
-application = VotrApp(settings=settings)
+    from votrfront.app import VotrApp
+    application = VotrApp(settings=settings)
+except Exception:
+    import os, sys, time, traceback
+    if not (__name__ == '__main__' and os.getenv('WERKZEUG_RUN_MAIN')): raise
+    traceback.print_exc()
+    print(" * Initialization failed, trying again in 5 seconds")
+    time.sleep(5)
+    sys.exit(3)   # return code used by werkzeug to request a reload
 
 if __name__ == '__main__':
     import sys
