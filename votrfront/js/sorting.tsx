7a0fbe643d693d5cadfcb816582136acac3fd74d
Tomi Belan <tomi.belan@gmail.com>
2022-12-26 00:48:38 +0100

    Separate implementations of sortTable and SortableTable.
    Also fix a bug where sortTable was ignoring defaultOrder, so some tables weren't
    sorted by default.

--- votrfront/js/sorting.tsx
+++ votrfront/js/sorting.tsx
@@ -39,13 +39,16 @@ export var sortAs = {
   },
 };
 
-export function sortTable<T>(
-  items: T[],
-  columns: Columns,
+function getOrder(
+  defaultOrder: string | null | undefined,
   query: Record<string, string>,
-  queryKey: string,
-  fullTable?: boolean
-): [T[], React.ReactNode] {
+  queryKey: string
+): string[] {
+  var orderString = query[queryKey] || defaultOrder;
+  return orderString ? orderString.split(/(?=[ad])/) : [];
+}
+
+function convertOldStyleColumns(columns: Columns): Columns {
   if (columns[0][0]) {
     columns = columns.map(
       ([label, prop, process, preferDesc, hiddenClass]) => ({
@@ -57,18 +60,28 @@ export function sortTable<T>(
       })
     );
   }
-  items = items.map((item, index) => ({ ...item, originalIndex: index }));
-  var orderString = query[queryKey] || columns.defaultOrder;
-  var order = orderString ? orderString.split(/(?=[ad])/) : [];
+  return columns;
+}
+
+function sortItems<T>(items: T[], columns: Columns, order: string[]): number[] {
   var directions = order.map((o) => (o.charAt(0) == "a" ? "asc" : "desc"));
   var iteratees = order.map((o) => {
     var { label, prop, process, preferDesc } = columns[Number(o.substring(1))];
-    return (item: T) =>
-      (process || _.identity)(prop ? (item as any)[prop] : item);
+    return (originalIndex: number) => {
+      var item = items[originalIndex];
+      return (process || _.identity)(prop ? (item as any)[prop] : item);
+    };
   });
+  return _.orderBy(_.range(items.length), iteratees, directions);
+}
 
-  items = _.orderBy(items, iteratees, directions);
-
+function renderHeader(
+  columns: Columns,
+  query: Record<string, string>,
+  queryKey: string,
+  order: string[],
+  fullTable: boolean
+): React.ReactNode {
   function handleClick(event: React.MouseEvent<HTMLElement>) {
     var index = event.currentTarget.getAttribute("data-index");
     var { label, prop, process, preferDesc } = columns[Number(index)];
@@ -83,7 +96,7 @@ export function sortTable<T>(
     navigate({ ...query, [queryKey]: newOrder.join("") });
   }
 
-  var header = (
+  return (
     <tr>
       {columns.map(
         (
@@ -107,8 +120,23 @@ export function sortTable<T>(
       )}
     </tr>
   );
+}
 
-  return [items, header];
+export function sortTable<T>(
+  items: T[],
+  columns: Columns,
+  query: Record<string, string>,
+  queryKey: string
+): [T[], React.ReactNode] {
+  var order = getOrder(columns.defaultOrder, query, queryKey);
+  columns = convertOldStyleColumns(columns);
+
+  var sortedIndexes = sortItems(items, columns, order);
+  var sortedItems = sortedIndexes.map((originalIndex) => items[originalIndex]);
+
+  var header = renderHeader(columns, query, queryKey, order, false);
+
+  return [sortedItems, header];
 }
 
 export function SortableTable<T>({
@@ -122,7 +150,7 @@ export function SortableTable<T>({
   expandedContentOffset = 0,
 }: {
   items: T[];
-  columns: Columns;
+  columns: Columns & { label: React.ReactNode }[];
   queryKey: string;
   withButtons?: boolean;
   footer?: (fullTable: boolean) => React.ReactNode;
@@ -146,13 +174,11 @@ export function SortableTable<T>({
 
   var fullTable = LocalSettings.get("fullTable") == "true";
 
-  const [sortedItems, header] = sortTable(
-    items,
-    columns,
-    query,
-    queryKey,
-    fullTable
-  );
+  var order = getOrder(columns.defaultOrder, query, queryKey);
+
+  var sortedIndexes = sortItems(items, columns, order);
+
+  var header = renderHeader(columns, query, queryKey, order, fullTable);
 
   const className = classNames(
     "table table-condensed table-bordered table-striped table-hover",
@@ -174,15 +200,17 @@ export function SortableTable<T>({
 
   const rows = [];
 
-  for (const item of sortedItems as (T & { originalIndex: number })[]) {
+  for (const originalIndex of sortedIndexes) {
+    const item = items[originalIndex];
+
     rows.push(
       <tr
-        key={item.originalIndex}
+        key={originalIndex}
         onClick={(event) => {
           // Don't toggle the row if we just clicked some link or input in the row.
           var target = event.target as Element;
           if (!target.closest("a, input, button") && !fullTable) {
-            toggleInfo(item.originalIndex);
+            toggleInfo(originalIndex);
           }
         }}
         className={rowClassName && rowClassName(item)}
@@ -210,9 +238,7 @@ export function SortableTable<T>({
                   className={classNames(
                     notExpandable,
                     "expand-arrow",
-                    open[item.originalIndex]
-                      ? "arrow-expanded"
-                      : "arrow-collapsed"
+                    open[originalIndex] ? "arrow-expanded" : "arrow-collapsed"
                   )}
                 />
               )}
@@ -225,15 +251,15 @@ export function SortableTable<T>({
 
     if (!fullTable) {
       rows.push(
-        <tr key={`${item.originalIndex}-striped-hack`} className="hidden" />
+        <tr key={`${originalIndex}-striped-hack`} className="hidden" />
       );
 
       rows.push(
         <tr
-          key={`${item.originalIndex}-info`}
+          key={`${originalIndex}-info`}
           className={classNames(
             notExpandable,
-            !open[item.originalIndex] && "hidden"
+            !open[originalIndex] && "hidden"
           )}
         >
           {expandedContentOffset > 0 && <td colSpan={expandedContentOffset} />}
