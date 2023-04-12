40aaea64f2aca6b0bfb8f1e3c1b4d7431c80035d
Tomi Belan <tomi.belan@gmail.com>
2023-04-12 12:26:28 +0200

    Add eslint-plugin-react-hooks.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "plugin:@typescript-eslint/strict",
+        "plugin:react-hooks/recommended",
       ],
       "overrides": [],
       "parser": "@typescript-eslint/parser",
